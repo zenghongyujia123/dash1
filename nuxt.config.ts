ffaa16db3547604acc879db55c1613b266e2665c
13472423583 <1963968619@qq.com>
2025-06-21 17:27:33 +0800

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  
+  devServer: {
+    port: 3001
+  },
 
   modules: [
     '@unocss/nuxt',
